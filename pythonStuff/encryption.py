8337a309ce34eae00ce7541adaf83f4aca0e59f3
JB-NAZ <nazariotjunior@gmail.com>
2023-12-28 17:41:22 +0900

    made an encryption base

--- pythonStuff/encryption.py
+++ pythonStuff/encryption.py
@@ -1,1 +1,12 @@
 #Code a basic incryption 
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#print(index)
+
+for char in text.lower(): 
+    
+    index = alphabet.find(char)
+    new_index = index + shift
+    encryption =  alphabet[new_index]
+    print("char:", char, "encryption:", encryption)
