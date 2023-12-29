c54bbc7b8fd54721ca0c62938a567643f2410b29
JB-NAZ <nazariotjunior@gmail.com>
2023-12-30 01:44:26 +0900

    Made it into a dice roullete

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Random dice = new Random();
 
-Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+string win = "You win";
+string lose = "You lose";
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine("Dice\tScores:");
+if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+{
+    Console.WriteLine("\nYou got +2 bonus points for rolling doubles!");
+    total = total + 2;
+}
+if (roll1 == roll2 && roll1 == roll3)
+{
+    Console.WriteLine("\nYou got +6 bonus points for rolling tripples!");
+    total = total + 6;
+}
+Console.WriteLine($"\nDice roll: {roll1} + {roll2} + {roll3} = {total}\n");
+
+if (total >= 15)
+{
+    Console.WriteLine(win);
+}
+else if (total < 15)
+{
+    Console.WriteLine(lose);
+}
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8beefd4139f97ba498d43c17a4688546d4dcaebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922482f4301c96afb085e0639ee91fcdfa9387db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
